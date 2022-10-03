ad48beb419aae58471f2dc58334e958b72cf8874
Alejandra Rubio <rubioalejandraf@gmail.com>
2022-10-02 23:19:01 -0300

    Projecto con Linq
    Practicando Linq varias query utilizando Query Sintax y Method Sintax.

---
+++ LabLINQ/LabLINQ.Logic/CustomerLogic.cs
@@ -0,0 +1,133 @@
+﻿using LabLINQ.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabLINQ.Logic
+{
+    public class CustomerLogic : BaseLogic
+    {
+        public List<Customers> EjercicioUno()
+        {
+            var queryCostumers = from c in _context.Customers select c;
+            return queryCostumers.ToList();
+        }
+        public List<Products> EjercicioDos()
+        {
+            var product = from p in _context.Products
+                          where p.UnitsInStock != 0
+                          select p;
+            return product.ToList();
+        }
+
+        public List<Products> EjercicioTres()
+        {
+            var product = from p in _context.Products
+                          where p.UnitsInStock != 0 && p.UnitPrice > 3
+                          select p;
+            return product.ToList();
+        }
+
+        public List<Customers> EjercicioCuatro()
+        {
+            var customer = _context.Customers.Where(c => c.Region.Equals("WA")).ToList();
+            return customer;
+        }
+
+        public void EjercicioCinco()
+        {
+            var productNulo789 = _context.Products.Where(p => p.ProductID.Equals(789)).FirstOrDefault();
+            if (productNulo789 == null)
+            {
+                Console.WriteLine("Producto 189 no existe");
+            }
+
+        }
+
+        public void EjercicioSeis()
+        {
+            var queryCostumers = from c in _context.Customers select c;
+            foreach (var c in queryCostumers)
+            {
+                Console.WriteLine($"ContactName= {c.ContactName.ToLower()} ContactName= {c.ContactName.ToUpper()}");
+            }
+        }
+
+        public void EjercicioSiete()
+        {
+
+            DateTime fecha = new DateTime(1997, 01, 01);
+            var customerOrders = from c in _context.Customers
+                                 join o in _context.Orders
+                                 on c.CustomerID equals o.CustomerID
+                                 where c.Region == "WA" && o.OrderDate > fecha
+                                 select new { c.Region, o.OrderDate };
+            foreach (var order in customerOrders)
+            {
+                Console.WriteLine($"{order}");
+            }
+        }
+        public void EjercicioOcho()
+        {
+            DateTime fecha = new DateTime(1997, 01, 01);
+            var custOrders = (from c in _context.Customers
+                              join o in _context.Orders
+                              on c.CustomerID equals o.CustomerID
+                              where c.Region == "WA" && o.OrderDate > fecha
+                              select new { c.Region, o.OrderDate }).Take(3);
+            foreach (var order in custOrders)
+            {
+                Console.WriteLine($"{order}");
+            }
+        }
+        public List<Products> EjercicioNueve()
+        {
+            var listProducto = _context.Products.OrderBy(p => p.ProductName).ToList();
+            return listProducto;
+
+        }
+        public List<Products> EjercicioDiez()
+        {
+            var listProductoStock = _context.Products.OrderByDescending(p => p.UnitsInStock
+            ).ToList();
+            return listProductoStock;
+
+        }
+        public void EjercicioOnce()
+        {
+            var categoriesProducts = from c in _context.Categories
+                                     join o in _context.Products
+                                     on c.CategoryID equals o.CategoryID
+                                     select new { c.CategoryName, o.ProductName };
+            foreach (var categorie in categoriesProducts)
+            {
+                Console.WriteLine($"{categorie}");
+            }
+        }
+        public void EjercicioDoce()
+        {
+            var productFirst = (from p in _context.Products select p).First();
+
+            Console.WriteLine($"Id = {productFirst.ProductID} - Producto= {productFirst.ProductName} - Precio= {productFirst.UnitPrice}");
+        }
+        public void EjercicioTrece()
+        {
+            var customerOrderCantidad = (from c in _context.Customers
+                                         join o in _context.Orders
+                                         on c.CustomerID equals o.CustomerID
+                                         group c by o.CustomerID into g 
+                                         
+                                         select new {g.Key}
+                                       );
+
+            foreach (var customer in customerOrderCantidad)
+            {
+                Console.WriteLine(customer);
+            }
+        }
+
+    }   
+}
---
+++ LabLINQ/LabLINQ.UI/Program.cs
@@ -0,0 +1,144 @@
+﻿using LabLINQ.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabLINQ.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            bool salir = false;
+            bool result;
+            int opcion2;
+            while (!salir)
+            {
+                CustomerLogic cl = new CustomerLogic();
+
+                Console.WriteLine("1. Ejercicio 1");
+                Console.WriteLine("2. Ejercicio 2");
+                Console.WriteLine("3. Ejercicio 3");
+                Console.WriteLine("4. Ejercicio 4");
+                Console.WriteLine("5. Ejercicio 5");
+                Console.WriteLine("6. Ejercicio 6");
+                Console.WriteLine("7. Ejercicio 7");
+                Console.WriteLine("8. Ejercicio 8");
+                Console.WriteLine("9. Ejercicio 9");
+                Console.WriteLine("10. Ejercicio 10");
+                Console.WriteLine("11. Ejercicio 11");
+                Console.WriteLine("12. Ejercicio 12");
+                Console.WriteLine("13. Ejercicio 13");
+                Console.WriteLine("14. Salir");
+                Console.WriteLine("Elige una de las opciones");
+
+               string opcion = (Console.ReadLine());
+
+                result = Int32.TryParse(opcion, out opcion2);
+                if (!result)
+                {
+                    System.Console.WriteLine("Invalido debes ingresat un numero");
+                }
+
+                Console.WriteLine("---------------------------------------------------------------------------");
+                switch (opcion2)
+                {
+                    case 1:
+                        foreach (var customerLogic in cl.EjercicioUno())
+                        {
+                            Console.WriteLine($"Address= {customerLogic.Address} ContactName= {customerLogic.ContactName}" +
+                                $"ContactTitle= {customerLogic.ContactTitle}  CompanyName={customerLogic.CompanyName}");
+                        }
+                     
+                        break;
+
+                    case 2:
+                        foreach (var customerLogic in cl.EjercicioDos())
+                        {
+                            Console.WriteLine($"ProductName= {customerLogic.ProductName} UnitsInStoc=" +
+                                $"{customerLogic.UnitsInStock}");
+                        }
+                     
+                        break;
+
+                    case 3:
+                        foreach (var customerLogic in cl.EjercicioTres())
+                        {
+                            Console.WriteLine($"ProductName= {customerLogic.ProductName} UnitsInStock= {customerLogic.UnitsInStock} " +
+                                $"UnitPrice= {customerLogic.UnitPrice}");
+                        }
+                       
+                        break;
+                    case 4:
+                        foreach (var customerLogic in cl.EjercicioCuatro())
+                        {
+                            Console.WriteLine($"Address= {customerLogic.Address} Region= {customerLogic.Region}");
+                        }
+                       
+                        break;
+                    case 5:
+                        cl.EjercicioCinco();
+                       
+                        break;
+
+                    case 6:
+                        cl.EjercicioSeis();
+                       
+                        break;
+
+                    case 7:
+                        cl.EjercicioSiete();
+                       
+                        break;
+                    case 8:
+                        cl.EjercicioOcho();
+                      
+                        break;
+                    case 9:
+                        foreach (var customerLogic in cl.EjercicioNueve())
+                        {
+                            Console.WriteLine($"ProductName= {customerLogic.ProductName}");
+                        }
+                     
+                        break;
+
+                    case 10:
+                        foreach (var customerLogic in cl.EjercicioDiez())
+                        {
+                            Console.WriteLine($"UnitsInStock= {customerLogic.UnitsInStock}");
+                        }
+                        
+                        break;
+
+                    case 11:
+                        cl.EjercicioOnce();
+                      
+                        break;
+                    case 12:
+                        cl.EjercicioDoce();
+                      
+                        break;
+                    case 13:
+                        cl.EjercicioTrece();
+                      
+                        break;
+                    case 14:
+                        Console.WriteLine("Chaoooooooooooo");
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Elige una opcion entre 1 y 13");
+                       
+                        break;
+                }
+                Console.WriteLine("---------------------------------------------------------------------------");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
